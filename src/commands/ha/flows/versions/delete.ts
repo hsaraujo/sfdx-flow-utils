5ad59b3ff8dd85f28c8fcc5c68d83b11e0d2d1b1
Heitor Araujo <heitaraujo@deloitte.co.nz>
2023-03-02 17:24:12 +1300

    fixes

--- src/commands/ha/flows/versions/delete.ts
+++ src/commands/ha/flows/versions/delete.ts
@@ -1,3 +1,5 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable no-console */
@@ -7,6 +9,7 @@
 /*
  * @author Heitor Araujo
  */
+import * as util from 'util';
 import { SfdxCommand, flags } from '@salesforce/command';
 import { Connection, Messages, SfdxError } from '@salesforce/core';
 import { AnyJson } from '@salesforce/ts-types';
@@ -26,6 +29,48 @@ interface Flow {
   Id: string;
 }
 
+export type Result = {
+  succeed: number;
+  failed: number;
+  detail: DeletionResponse[];
+};
+
+type CompositeRequest = {
+  method: string;
+  referenceId: string;
+  url: string;
+}
+
+type CompositeBody = {
+  allOrNone: boolean;
+  compositeRequest: CompositeRequest[];
+}
+
+type CompositeResponse = {
+  compositeResponse: CompositeSingleResponse[];
+}
+
+type CompositeSingleResponse = {
+
+  body: ErrorBody | string;
+  httpHeaders: unknown;
+  httpStatusCode: number;
+  referenceId: string;
+
+}
+
+type ErrorBody = {
+  message: string;
+  errorCode: string;
+  fields: unknown;
+}
+
+type DeletionResponse = {
+  id: string;
+  success: boolean;
+  errorMessage: string;
+}
+
 export default class Delete extends SfdxCommand {
   protected static requiresUsername = true;
   protected static requiresProject = false;
@@ -37,11 +82,51 @@ export default class Delete extends SfdxCommand {
       default: 0,
       required: true,
     }),
+    ignore: flags.array({
+      char: 'i',
+      description: messages.getMessage('ignoreFlagDescription'),
+      default: [],
+      required: false,
+    }),
   };
 
+  async run(): Promise<Result> {
+    const conn = this.org.getConnection();
+
+    this.ux.startSpinner('Deletion', 'Finding');
+    const obsoleteFlows = await this.findAllObsoleteVersions(conn);
+    const flowVersionsByDefinition = this.groupVersionsByDefinition(obsoleteFlows);
+    const versionsToDelete = this.filterVersionsToDelete(flowVersionsByDefinition);
+    this.ux.setSpinnerStatus('Deleting');
+    const CHUNK_SIZE = 25;
+    const result = [] as DeletionResponse[];
+    for (let i = 0; i <= versionsToDelete.length; i += CHUNK_SIZE) {
+      const start = i * CHUNK_SIZE;
+      const end = Math.min(versionsToDelete.length - start, CHUNK_SIZE) + start;
+      const subset = versionsToDelete.slice(start, end);
+      const responses = await this.runDeletions(subset, conn);
+      result.push(...responses);
+    }
+
+    this.ux.stopSpinner('Finished');
+
+    return Promise.resolve({
+      succeed: result.filter((item) => { return item.success }).length,
+      failed: result.filter((item) => { return !item.success }).length,
+      detail: result
+    });
+  }
+
   async findAllObsoleteVersions(conn: Connection): Promise<Flow[]> {
+    let query = 'SELECT Id, VersionNumber, DefinitionId, MasterLabel FROM Flow WHERE Status = \'Obsolete\' %s ORDER BY DefinitionId DESC, VersionNumber ASC';
+    let ignoreNameCondition = '';
+    if (this.flags.ignore && this.flags.ignore.length > 0) {
+      ignoreNameCondition = `AND Definition.DeveloperName NOT IN ('${this.flags.ignore.join('\', \'')}')`;
+    }
+    query = util.format(query, ignoreNameCondition);
+
     const result = await conn.tooling.query<Flow>(
-      "SELECT Id, VersionNumber, DefinitionId, MasterLabel FROM Flow WHERE Status = 'Obsolete' ORDER BY DefinitionId DESC, VersionNumber ASC"
+      query
     );
 
     if (!result.records || result.records.length <= 0) {
@@ -84,44 +169,44 @@ export default class Delete extends SfdxCommand {
     return versionsToDelete;
   }
 
-  async run(): Promise<AnyJson> {
-    const conn = this.org.getConnection();
-
-    let retry = true;
-    let previousVersionsToDelete = [];
-    let totalDeletedFlows = 0;
-
-    this.ux.startSpinner('Deletion', 'Finding');
-    do {
-      const obsoleteFlows = await this.findAllObsoleteVersions(conn);
-      const flowVersionsByDefinition = this.groupVersionsByDefinition(obsoleteFlows);
-      const versionsToDelete = this.filterVersionsToDelete(flowVersionsByDefinition);
-      if (totalDeletedFlows === 0) {
-        totalDeletedFlows = versionsToDelete.length;
+  async runDeletions(versionsToDelete: string[], conn: Connection): Promise<DeletionResponse[]> {
+    try {
+      const resp = await conn.tooling.request<CompositeResponse>({
+        method: 'POST',
+        url: '/services/data/v56.0/tooling/composite',
+        body: JSON.stringify(this.buildCompositeApiBody(versionsToDelete))
+      });
+      const deletionResponse = [] as  DeletionResponse[];
+      for(const singleResp of resp.compositeResponse){
+        deletionResponse.push(
+          {
+            id: singleResp.referenceId,
+            success: singleResp.httpStatusCode < 300 && singleResp.httpStatusCode >= 200,
+            errorMessage: (singleResp.body as ErrorBody).message || singleResp.body as string
+          }
+        )
       }
-      // this.ux.log(`Total versions to delete: ${versionsToDelete.length}`);
-      this.ux.setSpinnerStatus('Deleting');
-      try {
-        await conn.tooling.destroy<Flow[]>('Flow', versionsToDelete, { allOrNone: true });
-        retry = false;
-      } catch (ex) {
-        if (previousVersionsToDelete.length === versionsToDelete.length) {
-          totalDeletedFlows -= versionsToDelete.length;
-          retry = false;
-        } else {
-          this.ux.setSpinnerStatus('Retrying');
-        }
-        previousVersionsToDelete = Object.assign([], versionsToDelete);
-      }
-    } while (retry);
+      return Promise.resolve(deletionResponse);
+      // await conn.tooling.delete<Flow[]>('Flow', versionsToDelete, { allOrNone: false });
+    } catch (ex) {
+      throw new SfdxError(ex);
+    }
+  }
 
-    this.ux.stopSpinner('Finished');
-    this.ux.log(`Total deleted versions: ${totalDeletedFlows}`);
-    this.ux.styledObject({ 'Failed Versions': previousVersionsToDelete });
+  buildCompositeApiBody(versionsToDelete: string[]): CompositeBody{
+    
+    const compositeRequests = [] as CompositeRequest[];
+    for (const versionId of versionsToDelete) {
+      compositeRequests.push({
+        method: 'DELETE',
+        referenceId: versionId,
+        url: `/services/data/v56.0/tooling/sobjects/flow/${versionId}`
+      });
+    }
 
     return {
-      deleted: totalDeletedFlows,
-      failed: previousVersionsToDelete,
-    };
+      allOrNone: false,
+      compositeRequest: compositeRequests
+    }
   }
 }
